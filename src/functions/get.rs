5d4fb51a440c2a9cfba7b685768f9bd80064fca1
Kono <75730503+Kono-o@users.noreply.github.com>
2024-04-11 03:29:52 +0530

    Update .gitignore

--- src/functions/get.rs
+++ src/functions/get.rs
@@ -5,17 +5,14 @@ use std::path::{Path, PathBuf};
 
 const URL: &str = "https://github.com/InventivetalentDev/minecraft-assets/zipball/refs/heads/";
 
-pub fn try_get(v: &str, dir: &PathBuf) {
-    println!("{} {:?}", v, dir);
-}
-
-/*pub fn try_get(version: &str, dir: &PathBuf) {
+pub fn try_get(version: &str, dir: &PathBuf) {
     let url: String = format!("{}{}", URL, version);
-    let save_dir: PathBuf = dir.join(Path::new(&concat_string!("{}.zip", version)));
+    let save_dir: PathBuf = dir.join(Path::new(&concat_string!(version, ".zip")));
+    println!("{:?}", save_dir);
     let mut response = reqwest::blocking::get(url).unwrap();
     if response.status().as_u16() >= 300 {
         return;
     }
     let mut file = File::create(save_dir).unwrap();
     copy(&mut response, &mut file).unwrap();
-}*/
+}
