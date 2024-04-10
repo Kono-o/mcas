4ac937dd9bd2e48162730e4a09c2e9562acf44fb
Kono <75730503+Kono-o@users.noreply.github.com>
2024-04-10 23:22:47 +0530

    get func

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ const URL: &str = "https://github.com/InventivetalentDev/minecraft-assets/zipbal
 const SAVE_PATH: &str = "/media/kono/HDD/Coding/mcas/zips";
 
 fn main() {
-    let mut cur_dir: PathBuf;
+    let cur_dir: PathBuf;
     match std::env::current_dir() {
         Ok(dir) => cur_dir = dir,
         Err(err) => {
@@ -14,17 +14,32 @@ fn main() {
             return;
         }
     }
-
-    let func = std::env::args().nth(1).expect("no pattern given");
-    let ver = std::env::args().nth(2).expect("no path given");
-    
+    let func;
+    let ver;
+    match (std::env::args().nth(1), std::env::args().nth(2)) {
+        (Some(f), Some(v)) => {
+            func = f;
+            ver = v
+        }
+        _ => {
+            invalid_msg();
+            return;
+        }
+    }
+    match func.as_str() {
+        "get" => try_download(ver),
+        _ => {
+            invalid_msg();
+            return;
+        }
+    }
 }
 fn try_download(version: String) {
     match fs::create_dir(SAVE_PATH) {
         Ok(_) => println!("created {}", SAVE_PATH),
         _ => (),
     }
-    
+
     let url: String = format!("{}{}", URL, version);
     let file_dir: String = format!("{}{}.zip", SAVE_PATH, version);
     let mut response = reqwest::blocking::get(url).unwrap();
@@ -32,6 +47,9 @@ fn try_download(version: String) {
         return;
     }
     let mut file = File::create(file_dir).unwrap();
-    copy(&mut response, &mut file)?;
+    copy(&mut response, &mut file).unwrap();
 }
 
+fn invalid_msg() {
+    println!("invalid format, try <func> <version>")
+}
