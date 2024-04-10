ac9017a934c1b573ad0fb279b4d50af48dd1a1ce
Kono <75730503+Kono-o@users.noreply.github.com>
2024-04-11 04:48:45 +0530

    Update help.rs

--- src/functions/help.rs
+++ src/functions/help.rs
@@ -1,6 +1,6 @@
 use crate::log;
 
 pub fn help() {
-    log::msg("mcas get <version>          - to get the resource pack for that version");
+    log::msg("mcas get <version>     - to get the resource pack for that version");
     log::msg("mcas get 1.16.4");
 }
